d5d97a5ff9c58abb4bbac5928be5b2836c008b00
顾云翔 <guyunxiang32@gmail.com>
2017-04-09 18:34:25 +0800

    update

--- src/main.js
+++ src/main.js
@@ -17,3 +17,32 @@ new Vue({
 router.afterEach((rt) => {
   document.title = rt.meta.title || rt.name
 })
+
+
+// 消息队列
+const msgStack = {};
+
+// 校验消息是否已在队列中
+const validatorMsgStack = (msg, times = 0, type) => {
+  msgStack[type] = msgStack[type] || [];
+  const showing = msgStack[type].indexOf(msg) > -1;
+  if (showing) { return; }
+  msgStack[type].push(msg);
+  setTimeout(() => {
+    msgStack[type].splice(msgStack[type].indexOf(msg), 1);
+  }, parseFloat(times));
+  const wait = times || 1500;
+  Toast[type](msg, wait / 1000);
+};
+
+const _debounceToast = {
+  success: (msg, times) => {
+    validatorMsgStack(msg, times, 'success');
+  },
+  error: (msg, times) => {
+    validatorMsgStack(msg, times, 'error');
+  },
+  info: (msg, times) => {
+    validatorMsgStack(msg, times, 'info');
+  }
+};
